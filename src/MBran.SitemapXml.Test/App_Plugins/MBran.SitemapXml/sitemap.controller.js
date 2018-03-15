b36cbc0088b0b4d75d770c25c4a27dffc5a76e09
Mark Gil Libres <markglibres@gmail.com>
2018-03-15 17:40:25 +1100

    WIP: add datatype

---
+++ src/MBran.SitemapXml.Test/App_Plugins/MBran.SitemapXml/sitemap.controller.js
@@ -0,0 +1,7 @@
+﻿angular.module('umbraco')
+    .controller('MBran.SitemapXml.SitemapController', function ($scope) {
+        alert('test');
+        if (!$scope.model.value) {
+            $scope.model.value = {};
+        }
+});
