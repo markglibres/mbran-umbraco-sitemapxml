f76a1966a46d1132b7c92c0faadc97cd021d8f49
Mark Gil Libres <markglibres@gmail.com>
2018-03-16 10:44:57 +1100

    WIP: slider position

--- src/MBran.SitemapXml/App_Plugins/MBran.SitemapXml/sitemap.controller.js
+++ src/MBran.SitemapXml/App_Plugins/MBran.SitemapXml/sitemap.controller.js
@@ -3,12 +3,57 @@
         if (!$scope.model.value) {
             $scope.model.value = {};
         }
+        $scope.slider = {};
 
         $scope.clickFrequency = function(event) {
 
-            var x = event.offsetX ? event.offsetX : (event.originalEvent.layerX ? event.originalEvent.layerX : event.x);
-            var y = event.offsetY ? event.offsetY : (event.originalEvent.layerY ? event.originalEvent.layerY : event.y);
-
-            console.log(x, y);
+            var itemIndex = getItemIndex(event);
+            var sliderPosition = getSliderPosition(itemIndex);
+            setSliderPosition(sliderPosition);
         };
+
+        function init() {
+            $scope.frequencies = [
+                { value: 0, text: 'Always' },
+                { value: 1, text: 'Hourly' },
+                { value: 2, text: 'Daily' },
+                { value: 3, text: 'Weekly' },
+                { value: 4, text: 'Monthly' },
+                { value: 5, text: 'Yearly' },
+                { value: 6, text: 'Never' },
+            ];
+
+            $scope.itemPercent = 100 / $scope.frequencies.length;
+
+            var defaultPosition = getSliderPosition(0);
+            setSliderPosition(defaultPosition);
+        }
+
+        function getItemIndex(event) {
+            
+            var width = event.currentTarget.scrollWidth;
+            var clickedX = event.offsetX ? event.offsetX : (event.originalEvent.layerX ? event.originalEvent.layerX : event.x);
+            
+            var clickedPercentage = (clickedX / width) * 100;
+            var itemIndex = Math.ceil(clickedPercentage / $scope.itemPercent);
+            
+            return itemIndex;
+        }
+
+        function getSliderPosition(itemIndex) {
+            var left = (itemIndex * $scope.itemPercent) + ($scope.itemPercent / 2);
+
+            return {
+                left: left,
+                right: 100-left
+            };
+        }
+
+        function setSliderPosition(position) {
+
+            $scope.slider.left = position.left + '%';
+            $scope.slider.right = position.right + '%';
+        }
+
+        init();
     });
