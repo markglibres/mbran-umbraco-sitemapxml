c1a4f5dc33fbb748620245d6c89f1eaf66bd1115
Mark Gil Libres <markglibres@gmail.com>
2018-03-18 00:36:06 +1100

    fix sliders

--- src/MBran.SitemapXml.Test/App_Plugins/MBran.SitemapXml/sitemap.controller.js
+++ src/MBran.SitemapXml.Test/App_Plugins/MBran.SitemapXml/sitemap.controller.js
@@ -1,115 +1,64 @@
 ﻿angular.module('umbraco')
     .controller('MBran.SitemapXml.SitemapController', function ($scope, $element, assetsService, angularHelper) {
-        $scope.model.hideLabel = true;
         
-
-        if (!$scope.model.value) {
-            $scope.model.value = {};
-        }
-        $scope.model.value.priority = 3;
-        $scope.frequencySlider = {};
-        $scope.prioritySlider = {};
-
-        $scope.clickFrequency = function(event) {
-
-            var itemIndex = getItemIndex(event, $scope.frequencies);
-            var sliderPosition = getSliderPosition(itemIndex, $scope.frequencies);
-            setFrequencySliderPosition(sliderPosition);
-        };
-
-        $scope.clickPriority = function (event) {
-
-            console.log(event.offsetX);
-            var itemIndex = getItemIndex(event, $scope.priorities);
-            var sliderPosition = getSliderPosition(itemIndex, $scope.priorities);
-            
-            setPrioritySliderPosition(sliderPosition);
-        };
-
-        $scope.isPriority = function(priority) {
-            return $scope.model.value.priority === priority;
-        }
-
+        
         function init() {
-            $scope.frequencies = [
-                { value: 0, text: 'Always' },
-                { value: 1, text: 'Hourly' },
-                { value: 2, text: 'Daily' },
-                { value: 3, text: 'Weekly' },
-                { value: 4, text: 'Monthly' },
-                { value: 5, text: 'Yearly' },
-                { value: 6, text: 'Never' },
-            ];
 
-            $scope.priorities = [];
-
-            var priority = 0;
-            while (priority < 1) {
-                $scope.priorities.push({ value: priority, text: priority });
-                priority = +(priority + 0.1).toFixed(2);
-            }
-            
-                
-            if (!$scope.model.value.frequency) {
-                $scope.model.value.frequency = 2;
-            }
-            if (!$scope.model.value.priority) {
-                $scope.model.value.priority = 0.5;
-            }
-
-            $scope.priority = $scope.model.value.priority;
-
-            var defaultPosition = getSliderPosition($scope.model.value.frequency + 1, $scope.frequencies);
-            setFrequencySliderPosition(defaultPosition);
+            initOptions();
+            initModel();
+            initSliders();
         }
-
-        function getItemIndex(event, sliderItems) {
-            
-            var itemPercent = 100 / sliderItems.length;
-            var width = event.currentTarget.clientWidth;
-            var clickedX = event.offsetX; //event.clientX - width - event.currentTarget.offsetLeft;
-            console.log(clickedX);
-            var clickedPercentage = (clickedX / width) * 100;
-            var itemIndex = Math.ceil(clickedPercentage / itemPercent);
-            return itemIndex;
-        }
-
-        function getSliderPosition(itemIndex, sliderItems) {
-
-            var itemPercent = 100 / sliderItems.length;
-            var left = 0;
-            if (itemIndex === sliderItems.length) left = 100;
-            else if (itemIndex > 0) left = (itemIndex * itemPercent) - (itemPercent / 2);
-
-            return {
-                left: left,
-                right: 100-left
-            };
-        }
-
-        function setFrequencySliderPosition(position) {
-
-            $scope.frequencySlider.left = position.left + '%';
-            $scope.frequencySlider.right = position.right + '%';
-        }
-
-        function setPrioritySliderPosition(position) {
-
-            $scope.prioritySlider.left = position.left + '%';
-            $scope.prioritySlider.right = position.right + '%';
-        }
-
+        
         init();
 
+        function initOptions() {
+            $scope.model.hideLabel = true;
+            $scope.frequencies = ['Always', 'Hourly', 'Daily', 'Weekly', 'Monthly', 'Yearly', 'Never'];
+        }
+
+        function initModel() {
+            if (!$scope.model.value) {
+                $scope.model.value = {};
+            }
+
+            $scope.model.value.priority = $scope.model.value.priority ? parseFloat($scope.model.value.priority) : 0.5;
+            $scope.model.value.frequency = $scope.model.value.frequency ? parseInt($scope.model.value.frequency) : 2;
+        }
+
+        function initSliders() {
+            assetsService
+                .loadJs("~/Umbraco/lib/slider/js/bootstrap-slider.js")
+                .then(function () {
+
+                    createFrequencySlider();
+                    $scope.model.value.frequency.onValueChanged = function (newVal, oldVal) {
+                        if (newVal !== oldVal) {
+                            createFrequencySlider();
+                        }
+                    };
+
+                    createPrioritySlider();
+                    $scope.model.value.priority.onValueChanged = function (newVal, oldVal) {
+                        if (newVal !== oldVal) {
+                            createPrioritySlider();
+                        }
+                    };
+
+                });
+
+            assetsService.loadCss("~/Umbraco/lib/slider/bootstrap-slider.css");
+            assetsService.loadCss("~/Umbraco/lib/slider/bootstrap-slider-custom.css");
+        }
+
         function createPrioritySlider() {
-            var slider = $element.find('.priority-slider-item').bootstrapSlider({
-                ticks: "[1, 2, 3]",
-                ticks_labels: '["short", "medium", "long"]',
-                min:1,
-                max:3,
-                step:1,
+
+            $element.find('.priority-slider-item').bootstrapSlider({
+                min: 0,
+                max: 1,
+                step: 0.1,
                 value: $scope.model.value.priority,
-                tooltip:"hide"
+                tooltip: "always",
+                precision: 1
             }).on('slideStop', function (e) {
                 var value = e.value;
                 angularHelper.safeApply($scope, function () {
@@ -118,18 +67,27 @@
             }).data('slider');
         }
 
-        assetsService
-            .loadJs("~/Umbraco/lib/slider/js/bootstrap-slider.js")
-            .then(function () {
+        function createFrequencySlider() {
 
-                createPrioritySlider();
-
-                $scope.model.value.priority.onValueChanged = function (newVal, oldVal) {
-                    if (newVal != oldVal) {
-                        createPrioritySlider();
-                    }
-                };
-
+            var ticks = [];
+            
+            window.angular.forEach($scope.frequencies, function (value, key) {
+                ticks.push(key);
             });
-
+            
+            $element.find('.frequency-slider-item').bootstrapSlider({
+                ticks: ticks,
+                ticks_labels: $scope.frequencies,
+                min: ticks[0],
+                max: ticks[ticks.length-1],
+                step: 1,
+                value: $scope.model.value.frequency,
+                tooltip: "hide"
+            }).on('slideStop', function (e) {
+                var value = e.value;
+                angularHelper.safeApply($scope, function () {
+                    $scope.model.value.frequency = value;
+                });
+            }).data('slider');
+        }
     });
